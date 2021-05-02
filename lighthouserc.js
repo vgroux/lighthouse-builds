1c6e404a87f735795179755ce54686a0867fd873
Vincent GROUX <vincent.groux@gmail.com>
2021-05-02 11:30:20 +0200

    Bumping up max wait for FCP

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,8 @@ module.exports = {
       settings: { 
         chromeFlags: '--no-sandbox',
         // Don't run accessibility category
-        onlyCategories: ["performance"]
+        onlyCategories: ["performance", "seo", "best-practices", "pwa"],
+        maxWaitForFcp: 60 * 1000,
       },
       url: ["https://nationalpost.com"]
     },
